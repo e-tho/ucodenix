a7a4e5b5cca9dd9daec0a157bed20d39574bd324
e-tho <128100160+e-tho@users.noreply.github.com>
2024-11-13 16:12:05 +0100

    Bump version to 1.2.0

--- flake.nix
+++ flake.nix
@@ -4,49 +4,49 @@
   outputs = { self, ... }:
     let
       ucodenix =
-      { stdenv
-      , lib
-      , fetchFromGitHub
-      , amd-ucodegen
-      , cpuid
+        { stdenv
+        , lib
+        , fetchFromGitHub
+        , amd-ucodegen
+        , cpuid
 
-      , cpuModelId
-      }: stdenv.mkDerivation {
-        pname = "ucodenix";
-        version = "1.1.0";
+        , cpuModelId
+        }: stdenv.mkDerivation {
+          pname = "ucodenix";
+          version = "1.2.0";
 
-        src = fetchFromGitHub {
-          owner = "platomav";
-          repo = "CPUMicrocodes";
-          rev = "7d439ddf43c75a9ef410b40608c0cb545d722c30";
-          hash = "sha256-cpFAvnLg0OSH+sa0EkojovGRui5joV2cDcFEd2Rnqts=";
+          src = fetchFromGitHub {
+            owner = "platomav";
+            repo = "CPUMicrocodes";
+            rev = "7d439ddf43c75a9ef410b40608c0cb545d722c30";
+            hash = "sha256-cpFAvnLg0OSH+sa0EkojovGRui5joV2cDcFEd2Rnqts=";
+          };
+
+          nativeBuildInputs = [ amd-ucodegen ] ++ lib.optionals (cpuModelId == "auto") [ cpuid ];
+
+          buildPhase = ''
+            if [ "${cpuModelId}" = "auto" ]; then
+              modelResult=$(cpuid -1 -l 1 -r | sed -n 's/.*eax=0x\([0-9a-f]*\).*/\U\1/p')
+            else
+              modelResult="${cpuModelId}"
+            fi
+            find $src/AMD -name "cpu$modelResult*.bin" -exec cp {} microcode.bin \;
+
+            if [ ! -f microcode.bin ]; then
+              echo "No microcode found with model $modelResult"
+              exit 1
+            fi
+
+            mkdir -p $out/kernel/x86/microcode
+            amd-ucodegen -o $out/kernel/x86/microcode/AuthenticAMD.bin microcode.bin
+          '';
+
+          meta = {
+            description = "Generated AMD microcode for CPU";
+            license = lib.licenses.gpl3;
+            platforms = lib.platforms.linux;
+          };
         };
-
-        nativeBuildInputs = [ amd-ucodegen ] ++ lib.optionals (cpuModelId == "auto") [ cpuid ];
-
-        buildPhase = ''
-          if [ "${cpuModelId}" = "auto" ]; then
-            modelResult=$(cpuid -1 -l 1 -r | sed -n 's/.*eax=0x\([0-9a-f]*\).*/\U\1/p')
-          else
-            modelResult="${cpuModelId}"
-          fi
-          find $src/AMD -name "cpu$modelResult*.bin" -exec cp {} microcode.bin \;
-
-          if [ ! -f microcode.bin ]; then
-            echo "No microcode found with model $modelResult"
-            exit 1
-          fi
-
-          mkdir -p $out/kernel/x86/microcode
-          amd-ucodegen -o $out/kernel/x86/microcode/AuthenticAMD.bin microcode.bin
-        '';
-
-        meta = {
-          description = "Generated AMD microcode for CPU";
-          license = lib.licenses.gpl3;
-          platforms = lib.platforms.linux;
-        };
-      };
 
     in
     {

b9aa3738bc367d9f653f556e42d8de0af377470e
e-tho <128100160+e-tho@users.noreply.github.com>
2024-12-13 14:24:58 +0100

    Bump version to 1.3.0

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         , cpuModelId
         }: stdenv.mkDerivation {
           pname = "ucodenix";
-          version = "1.2.0";
+          version = "1.3.0";
 
           src = cpu-microcodes;
 

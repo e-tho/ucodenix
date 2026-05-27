d78223a695688c0f44f70ba8b063a5debb5fd1dc
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-05-28 05:57:26 +0700

    extract microcode-amd-ucodenix callPackage fn to pkgs/microcode-amd-ucodenix/default.nix

--- flake.nix
+++ flake.nix
@@ -10,24 +10,7 @@
     { self, cpu-microcodes, ... }:
     let
 
-      microcode-amd-ucodenix =
-        {
-          stdenv,
-          libarchive,
-          ucodenix,
-        }:
-        stdenv.mkDerivation {
-          name = "amd-ucode";
-          src = ucodenix;
-
-          nativeBuildInputs = [ libarchive ];
-
-          installPhase = ''
-            mkdir -p $out
-            touch -d @$SOURCE_DATE_EPOCH kernel/x86/microcode/AuthenticAMD.bin
-            echo kernel/x86/microcode/AuthenticAMD.bin | bsdtar --uid 0 --gid 0 -cnf - -T - | bsdtar --null -cf - --format=newc @- > $out/amd-ucode.img
-          '';
-        };
+      microcode-amd-ucodenix = ./pkgs/microcode-amd-ucodenix;
 
     in
     {
